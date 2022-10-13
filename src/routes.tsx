334a38e4d4a42efcbf0935689c08617dc3ef6c3f
Jorge Edo. Hernández <jorge@galpha.co>
2022-10-13 10:37:47 -0300

    Tailwind Examples

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { Home as HomeTailwind } from "./pages/Tailwind/home";
+import { Login as LoginTailwind } from "./pages/Tailwind/login";
 
 export function Rotas(){
   return(
@@ -8,6 +10,8 @@ export function Rotas(){
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/tailwind/home' element={<HomeTailwind/>} />
+        <Route path='/tailwind/login' element={<LoginTailwind/>} />
       </Routes>
     </BrowserRouter>
   )
